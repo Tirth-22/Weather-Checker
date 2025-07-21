def25f7f4aa1d780d649f9670dbcb50e5ea2cca1
Tirth Makadia <tirthmakadia2006@gmail.com>
2025-07-22 00:29:25 +0530

    Final for run Main.java

--- Weather/Main.java
+++ Weather/Main.java
@@ -1,32 +1,8 @@
 package Weather;
 
-import java.util.Scanner;
-
-public class Main {
+public class Main extends JavaSwing1{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Weather Information System");
-        System.out.println("-------------------------");
 
-        while (true) {
-            System.out.print("\nEnter city name (or 'quit' to exit): ");
-            String city = scanner.nextLine().trim();
-
-            if (city.equalsIgnoreCase("quit")) {
-                break;
-            }
-
-            if (city.isEmpty()) {
-                System.out.println("Please enter a valid city name.");
-                continue;
-            }
-
-            System.out.println("\nFetching weather data...");
-            String result = WeatherFetcher.getWeather(city);
-            System.out.println("\n" + result);
-        }
-
-        scanner.close();
-        System.out.println("\nThank you for using the Weather Information System!");
+        JavaSwing1 js = new JavaSwing1();
     }
 }
